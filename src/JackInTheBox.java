7ff6a6ed96628a0e8434a05eba4d3d488cb04198
AnneJTL <anne.chatin@jointheleague.org>
2018-09-17 10:57:20 -0700

    Jack In The Box

---
+++ src/JackInTheBox.java
@@ -0,0 +1,132 @@
+import java.applet.AudioClip;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class JackInTheBox {
+	
+	private JFrame maFenetre;
+	private JButton monBouton;
+	private int compteur = 0;
+	private JLabel monLabel;
+	
+
+	public JackInTheBox() {
+		prepareGUI();		
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JackInTheBox jack = new JackInTheBox();
+
+	}
+
+	private void prepareGUI(){
+		// Creation GUI
+		maFenetre = new JFrame();
+		maFenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		maFenetre.setTitle("Jack In The Box");
+		
+		monBouton = new JButton("Surprise !");
+		monBouton.setSize(80, 30);
+		maFenetre.add(monBouton);
+			
+		maFenetre.setSize(400, 400);
+		maFenetre.setVisible(true);
+
+		monBouton.addMouseListener(new monListener());
+	}
+	
+	class monListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("Clicked");
+			compteur++;
+			if (compteur == 5){
+				compteur = 0;
+				playSound("homer-woohoo.wav");
+				showPicture("jackInTheBox.png");
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("mouseEntered");
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("mouseExited");
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("mousePressed");
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("mouseReleased");
+
+		}
+		
+	}
+	
+	private void showPicture(String fileName) { 
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+	
+	private void playSound(String soundFile) { 
+	     try {
+	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+	          sound.play();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+}
